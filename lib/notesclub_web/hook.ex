e1ebf8771e369b25775fc64de9edd012bacdf244
specv <61646426+specv@users.noreply.github.com>
2023-02-18 21:33:59 +0800

    Add `log_context?` option to `life_cycle_hook`

--- lib/notesclub_web/hook.ex
+++ lib/notesclub_web/hook.ex
@@ -12,6 +12,7 @@ defmodule LifeCycleHook do
     only = Keyword.get(opts, :only)
     except = Keyword.get(opts, :except)
     log_level = Keyword.get(opts, :log_level, :debug)
+    log_context? = Keyword.get(opts, :log_context?, true)
 
     stages =
       case {only, except} do
@@ -22,73 +23,78 @@ defmodule LifeCycleHook do
       end
 
     quote do
-      on_mount({unquote(__MODULE__), %{stages: unquote(stages), log_level: unquote(log_level)}})
+      on_mount({unquote(__MODULE__), %{stages: unquote(stages), log_level: unquote(log_level), log_context?: unquote(log_context?)}})
     end
   end
 
-  def on_mount(%{stages: stages, log_level: log_level}, params, session, socket) do
+  def on_mount(%{stages: stages, log_level: log_level, log_context?: log_context?}, params, session, socket) do
     socket =
       stages
       |> Enum.reduce(socket, fn stage, socket ->
         case stage do
           :mount ->
-            log_mount_life_cycle(socket, params, session, log_level)
+            log_mount_life_cycle(socket, params, session, log_level, log_context?)
 
             socket
 
           stage ->
-            socket |> attach_life_cycle_hook(stage, log_level)
+            socket |> attach_life_cycle_hook(stage, log_level, log_context?)
         end
       end)
 
     {:cont, socket}
   end
 
-  defp log_mount_life_cycle(socket, _params, _session, log_level) do
+  defp log_mount_life_cycle(socket, params, session, log_level, log_context?) do
     message =
       [get_common_message(socket, :mount), get_connection_message(socket)]
       |> Enum.join(" ")
 
-    Logger.log(log_level, message, ansi_color: :red)
+    log(log_level, message, log_context?, %{params: params, session: session, socket: socket})
   end
 
-  defp attach_life_cycle_hook(socket, :handle_params, log_level) do
+  defp attach_life_cycle_hook(socket, :handle_params, log_level, log_context?) do
     socket
-    |> attach_hook(:life_cycle_hook, :handle_params, fn _params, _uri, socket ->
+    |> attach_hook(:life_cycle_hook, :handle_params, fn params, uri, socket ->
       message =
         [get_common_message(socket, :handle_params), get_connection_message(socket)]
         |> Enum.join(" ")
 
-      Logger.log(log_level, message, ansi_color: :red)
+      log(log_level, message, log_context?, %{params: params, uri: uri, socket: socket})
 
       {:cont, socket}
     end)
   end
 
-  defp attach_life_cycle_hook(socket, :handle_event, log_level) do
+  defp attach_life_cycle_hook(socket, :handle_event, log_level, log_context?) do
     socket
-    |> attach_hook(:life_cycle_hook, :handle_event, fn event, _params, socket ->
+    |> attach_hook(:life_cycle_hook, :handle_event, fn event, params, socket ->
       message =
         [get_common_message(socket, :handle_event), "event: #{event}"]
         |> Enum.join(" ")
 
-      Logger.log(log_level, message, ansi_color: :red)
+      log(log_level, message, log_context?, %{event: event, params: params, socket: socket})
 
       {:cont, socket}
     end)
   end
 
-  defp attach_life_cycle_hook(socket, :handle_info, log_level) do
+  defp attach_life_cycle_hook(socket, :handle_info, log_level, log_context?) do
     socket
     |> attach_hook(:life_cycle_hook, :handle_info, fn message, socket ->
       message =
         [get_common_message(socket, :handle_info), "message: #{message}"]
         |> Enum.join(" ")
 
-      Logger.log(log_level, message, ansi_color: :red)
+      log(log_level, message, log_context?, %{message: message, socket: socket})
 
       {:cont, socket}
     end)
+  end
+
+  defp log(log_level, message, log_context?, context) do
+      Logger.log(log_level, message, ansi_color: :red)
+      if log_context? and Logger.compare_levels(Logger.level(), log_level) != :gt, do: dbg(context)
   end
 
   defp get_connection_message(socket) do
